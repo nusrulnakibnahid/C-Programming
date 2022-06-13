da88763521f8a272107bc71404ebd078ff87adcf
Md.Nusrul Nakib Nahid <105875914+nahid1054@users.noreply.github.com>
2022-06-13 20:45:20 +0600

    Add files via upload

---
+++ Home Work 1/program3.c
@@ -0,0 +1,29 @@
+/////calculate + - * / % operator in one program with/without user input
+#include<stdio.h>
+int main (){
+	double x,y,addition,substraction,multiply,division,modulus;
+	
+
+printf("enter two number=");
+scanf("%lf %lf",&x,&y);
+
+addition=x+y;
+printf("This is add=%lf\n",addition);
+
+substraction=x-y;
+printf("This is sub=%lf\n",substraction);
+
+multiply=x*y;
+printf("This is mul=%lf\n",multiply);
+
+
+division=x/y;
+printf("This is div=%lf\n",division);
+
+
+
+
+return 0;
+	
+	
+}
