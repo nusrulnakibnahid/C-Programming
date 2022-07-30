7c57c9842b1ec3c72c5a57080ce9dc20bb1061b6
Md.Nusrul Nakib Nahid <105875914+nahid1054@users.noreply.github.com>
2022-07-30 22:39:33 +0600

    Add files via upload

---
+++ Conditional Statements/odd_even.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int num;
+	printf("Enter any number=");
+	scanf("%d",&num);
+	
+	if(num%2==0){
+	
+	printf("this number is even");
+	
+	}
+	
+	else {
+	
+	printf("this number is odd");	
+	
+	}
+	
+	return 0;
+}
---
+++ Conditional Statements/pass-fail if_else.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int math;
+	printf("Enter the marks=");
+	scanf("%d",&math);
+	
+	if(math>40){
+	
+	printf("Student Pass");
+	
+	}
+	
+	else {
+	
+	printf("Student fail");	
+	
+	}
+	
+	
+
+	return 0;
+}
